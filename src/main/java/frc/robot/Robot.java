d8adc0eb146666abe8b7e2940ef41676f26a03f2
Chris Voon <christopher.voon@gmail.com>
2025-01-17 16:06:36 -0800

    Remove duplicate BuildConstants file

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,7 +19,6 @@ import com.ctre.phoenix6.swerve.SwerveModuleConstants.SteerMotorArrangement;
 import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.constants.BuildConstants;
 import frc.robot.constants.Constants;
 import frc.robot.generated.TunerConstants;
 import org.littletonrobotics.junction.LogFileUtil;
